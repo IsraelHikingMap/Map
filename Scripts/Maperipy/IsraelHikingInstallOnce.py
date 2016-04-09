80a8bb76441a342bfd7e6b24c4696d687335a209
zstadler <zeev.stadler@gmail.com>
2016-04-09 14:28:14 +0300

    Perform one-time installation tasks
    1. Copy DEM files to Maperitive installation
    2. Copy additional icons to the Maperitive installation
    
    Resolve "Copy icons and hgt files to Maperitive in installation script"
    - #13

---
+++ Scripts/Maperipy/IsraelHikingInstallOnce.py
@@ -0,0 +1,23 @@
+#
+# Perform one-time installation tasks:
+# 1. Copy DEM files to Maperitive installation
+# 2. Copy additional icons to the Maperitive installation
+#
+
+import os.path
+from maperipy import *
+
+# http://stackoverflow.com/questions/749711/how-to-get-the-python-exe-location-programmatically
+MaperitiveDir = os.path.dirname(os.path.dirname(os.path.normpath(os.__file__)))
+App.log('MaperitiveDir: ' + MaperitiveDir)
+IsraelHikingDir = os.path.dirname(os.path.dirname(os.path.normpath(App.script_dir)))
+App.log('IsraelHikingDir: ' + IsraelHikingDir)
+
+#
+# TODO: The following was tested only on MS-Windows platforms
+#
+App.log("Copy missing DEM files, if any, to the Maperitive Installation")
+App.run_program('xcopy.exe', 1200, ["/S/I/D/F/R/Y", '"'+os.path.join(IsraelHikingDir, "Cache", "Rasters")+'"', '"'+os.path.join(MaperitiveDir, "Cache", "Rasters")+'"'])
+
+App.log("Copy missing Icons, if any, to the Maperitive Installation")
+App.run_program('xcopy.exe', 1200, ["/S/I/D/F/R/Y", '"'+os.path.join(IsraelHikingDir, "Icons")+'"', '"'+os.path.join(MaperitiveDir, "icons")+'"'])
