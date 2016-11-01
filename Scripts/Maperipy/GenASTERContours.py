7398b197d5bcca03a72ec65fb740247096232502
zstadler <zeev.stadler@gmail.com>
2016-11-02 00:16:12 +0200

    Remove bathimetry information from SRTM HGT files
    Resolve #102
    Restore sea-level inland contours in MTB map

--- Scripts/Maperipy/GenASTERContours.py
+++ Scripts/Maperipy/GenASTERContours.py
@@ -7,13 +7,13 @@ import sys
 
 # http://stackoverflow.com/questions/749711/how-to-get-the-python-exe-location-programmatically
 MaperitiveDir = os.path.dirname(os.path.dirname(os.path.normpath(os.__file__)))
-# App.log('MaperitiveDir: ' + MaperitiveDir)
+# App.log('MaperitiveDir: '+MaperitiveDir)
 ProgramFiles = os.path.normpath(os.path.dirname(MaperitiveDir))
-# App.log('ProgramFiles: ' + ProgramFiles)
+# App.log('ProgramFiles: '+ProgramFiles)
 IsraelHikingDir = os.path.dirname(os.path.dirname(os.path.normpath(App.script_dir)))
-# App.log('App.script_dir: ' + App.script_dir)
-# App.log('IsraelHikingDir: ' + IsraelHikingDir)
-App.run_command('change-dir dir="' + IsraelHikingDir +'"')
+# App.log('App.script_dir: '+App.script_dir)
+# App.log('IsraelHikingDir: '+IsraelHikingDir)
+App.run_command('change-dir dir="'+IsraelHikingDir +'"')
 os.chdir(IsraelHikingDir)
 
 # Set bounds to match
@@ -21,47 +21,28 @@ os.chdir(IsraelHikingDir)
 App.run_command("set-geo-bounds 34.07929,29.37711,35.91531,33.35091");
 
 # Geo bounds in integer degrees
-MinLon=int(Map.geo_bounds.min_x)
-MaxLon=int(math.ceil(Map.geo_bounds.max_x))
+MinLon=Map.geo_bounds.min_x
+MaxLon=Map.geo_bounds.max_x
 
 MinLat=int(Map.geo_bounds.min_y)
 MaxLat=int(math.ceil(Map.geo_bounds.max_y))
 
-App.run_command('set-dem-source ASTER')
+HGTDir = 'ASTER'
+
+App.run_command('set-dem-source '+HGTDir)
 App.run_command('set-setting name=scripts.completion-signal.min-duration-sec value=99999')
 
-App.log("Generating contours "+str(MinLon)+'-'+str(MinLat)+" to "+str(MaxLon)+'-'+str(MaxLat) )
+App.log("Generating contours {}-{} to {}-{}".format(MinLon, MinLat, MaxLon, MaxLat))
 
 for Lat in range (MinLat, MaxLat, 1) :
-  for Lon in range (MinLon, MaxLon, 1) :
-    App.run_command('generate-contours interval=10 min-ele=-380 bounds='+str(Lon)+','+str(Lat)+ ',' +str(Lon+1)+','+str(Lat+1))
-    Quad = str(Lon)+'-'+str(Lat)
-    Contours = os.path.join('Cache', 'ASTER-'+Quad+'.contours')
-    App.run_command('save-source ' + Contours)
-    App.run_command('remove-source')
+#  for Lon in range (MinLon, MaxLon, 1) :
+    App.run_command('clear-map')
+    App.run_command('generate-contours interval=10 bounds={},{},{},{}'
+                    .format(MinLon,
+                            max(Lat, Map.geo_bounds.min_y),
+                            MaxLon,
+                            min(Lat+1, Map.geo_bounds.max_y)))
+    App.run_command('save-source ' 
+                    + os.path.join('Cache', '{}-N{}.contours'.format(HGTDir, Lat)))
 
-    # Remove shore and off-shore contour lines
-    try:
-      Conn = None
-      Conn = sqlite3.connect(Contours)
-      App.log("Analysing the contours of " + Contours)
-      Cur = Conn.cursor()
-      if Quad == "35-32":
-	# Sea of Galilee
-	Cur.execute("DELETE FROM Contours WHERE Elevation <= -210 AND MinY>32.7;")
-	App.log("Removed "+str(Cur.rowcount)+" shore and off-shore contour lines")
-      elif Quad in set(["35-30", "35-31"]):
-	# Jordan Valley
-	App.log("Removed "+str(Cur.rowcount)+" shore and off-shore contour lines")
-      else:
-	# Elsewhere
-	Cur.execute("DELETE FROM Contours WHERE Elevation <= 0;")
-	App.log("Removed "+str(Cur.rowcount)+" shore and off-shore contour lines")
-      Conn.commit()
-      Cur.close()
-      App.log("Changes comitted")
-    except sqlite3.Error, e:
-      print "Error %s:" % e.args[0]
-      sys.exit(1)
-
-# App.run_command('save-map-script file=' + os.path.join('Scripts', 'Contours.mscript'))
+# vim: shiftwidth=4 expandtab
