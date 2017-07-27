65721c425c823cb3adf9b2a3354678123a05e4f3
zstadler <zeev.stadler@gmail.com>
2017-07-27 23:29:18 +0300

    Another fix for handling Non/Incremantal and Not/Continuing OverlayTiles phase

--- Scripts/Maperipy/CreateAllMaps.py
+++ Scripts/Maperipy/CreateAllMaps.py
@@ -164,7 +164,7 @@ if remainingPhases == []:
 #
 App.run_command("use-ruleset location="+os.path.join("Rules", "empty.mrules"))
 
-if osm_source.status() in ["non-incremental", "incremental"]:
+if osm_source.status() in ("non-incremental", "incremental"):
     # Continue an incomplete run
     App.log('=== Continuing execution of the previous tile generation ===')  
     App.log('Remaining phases: '+', '.join(remainingPhases))
@@ -229,7 +229,7 @@ if remainingPhases:
             pass
 
     # Tile generation
-    if [val for val in ['IsraelHiking15', 'IsraelMTB15', 'IsraelHiking16', 'IsraelMTB16']
+    if [val for val in ('IsraelHiking15', 'IsraelMTB15', 'IsraelHiking16', 'IsraelMTB16')
             if val in remainingPhases]:
         App.run_command("run-script file="+os.path.join("Scripts", "Maperitive", "IsraelDecoration.mscript"))
 
@@ -290,11 +290,14 @@ if remainingPhases:
         App.log("=== Create Trails Overlay tiles ===")
         App.run_command("clear-map")
         App.run_command("use-ruleset location="+os.path.join("Rules", "empty.mrules"))
-        changed = False
+        if osm_trails.status() in ("uninitialized", "base"):
+            # Not continuing execution of the previous tile generation 
+            if osm_trails.downloadMap():
+                raise RuntimeError
         if osm_trails.status() == "non-incremental":
             Map.add_osm_source(osm_trails.updated)
             changed = True
-        elif not osm_trails.downloadMap():
+        else:
             trails_overlay.osmChangeRead(osm_trails.changes, osm_trails.base, osm_trails.updated)
             (changed, guard) = trails_overlay.statistics(False)
         App.collect_garbage()
