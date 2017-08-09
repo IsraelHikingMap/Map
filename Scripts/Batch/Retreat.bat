2cc9bdf8560bd988eddee46616cd23f4dcda1584
zstadler <zeev.stadler@gmail.com>
2017-08-09 23:21:38 +0300

    Add reatreat.bat
    Enable selective non-incremental tile generation

---
+++ Scripts/Batch/Retreat.bat
@@ -0,0 +1,67 @@
+@REM Israel Hiking and Biking - IsraelHiking.osm.org.il
+@REM
+@REM Usage: retreat [Language] [Phase ...]
+@REM
+@REM Enable selective non-incremental tile generation by seting the
+@REM status of the Cache for a given language as if tile generation
+@REM was just completed, before calling advance() and the deletion of
+@REM the *.done files
+@REM
+@REM Optional arguments:
+@REM 
+@REM   Language: Either Hebrew or English (case insensitive).
+@REM       Default: Hebrew
+@REM   Phase: Space-separated list of phases to be enabled after the
+@REM       retreat.
+@REM       Substrings such as MTB and 16 are accepted. 
+@REM       Match is case insensitive.
+@REM       Default: all phases are enabled
+@REM
+@REM Copyright 2017 Zeev Stadler CC BY-NC-SA 3.0
+@REM https://creativecommons.org/licenses/by-nc-sa/3.0/
+@ECHO OFF
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET LANGUAGE="Hebrew"
+ECHO " Hebrew English " | FIND /I " %~1 " > NUL && (
+  SET LANGUAGE="%~1"
+  SHIFT
+)
+
+@REM Script is located at IsraelHiking\Scripts\Batch 
+PUSHD "%~dp0"\..\..\Cache\%LANGUAGE%
+
+IF EXIST "israel-and-palestine-updated.osm.pbf" (
+  DIR
+  ECHO.
+  ECHO === Tile generation was not completed, cannot un-advance ===
+  POPD
+  EXIT /B 1
+)
+
+IF EXIST israel-and-palestine-latest.osm.pbf (
+  REN israel-and-palestine-latest.osm.pbf israel-and-palestine-updated.osm.pbf
+)
+IF EXIST israel-and-palestine-*trails* (
+  DEL israel-and-palestine-*trails*
+)
+
+SET ALLPHASES=IsraelHiking15 IsraelMTB15 IsraelHiking16 IsraelMTB16
+IF /I %LANGUAGE% == "Hebrew" (
+  SET ALLPHASES=%ALLPHASES% OverlayTiles
+)
+
+IF NOT "%*"=="" (
+  FOR %%P in ( %ALLPHASES% ) DO (
+    (CALL) > %%P.done
+    FOR %%M in ( %* ) DO (
+      ECHO %%P | FINDSTR /I %%M > NUL && DEL %%P.done
+    )
+  )
+)
+
+DIR
+POPD
+EXIT /B
+@REM vim:sw=2:ai:ic:expandtab
